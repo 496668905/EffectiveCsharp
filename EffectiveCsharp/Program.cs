e1427b9a755259d43423af32274c2face5ebf2c4
许杰 <496668905@qq.com>
2020-08-16 23:21:23 +0800

    添加项目文件。

---
+++ EffectiveCsharp/Program.cs
@@ -0,0 +1,47 @@
+﻿using EffectiveCsharp._1To25;
+using System;
+using static System.Console;
+namespace EffectiveCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Choujiang c = new Choujiang();
+            c.cj += c_ThresholdReached;
+
+            Console.WriteLine("press 'a' key to increase total");
+            string name = "";
+            while ((name=Console.ReadLine())!="")
+            {
+                c.CjFun(name);
+            }
+        }
+
+        static void c_ThresholdReached(object sender, CjEventArg e)
+        {
+            Console.WriteLine($"{e.date}恭喜你中奖了");
+            Environment.Exit(0);
+        }
+    }
+
+    class Choujiang
+    {
+        public event EventHandler<CjEventArg> cj;
+        public void CjFun(string name)
+        {
+            if (name == "xx")
+            {
+                CjEventArg cc = new CjEventArg();
+                cc.date = DateTime.Now;
+                cj.Invoke(this,cc);
+               
+            }
+        }
+    }
+
+    class CjEventArg : EventArgs
+    {
+        public DateTime date;
+    }
+}
