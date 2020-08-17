e1694a92ec6fd0e57f426a118c624b1de47f38d4
许杰 <496668905@qq.com>
2020-08-18 00:10:14 +0800

    14条

---
+++ EffectiveCsharp/1To25/t14.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EffectiveCsharp._1To25
+{
+    /// <summary>
+    /// 尽量删减重复的初始化逻辑
+    /// </summary>
+    public class t14
+    {
+        private int count;
+        private string name;
+        public string copyName;
+        public t14() : this(0, "都选C")
+        {
+
+        }
+        public t14(int count = 0, string name = "")
+        {
+            this.count = count;
+            this.name = name;
+            copyName = name;
+            if (this.count < 0)
+            {
+                this.count = 0;
+            }
+        }
+    }
+}
--- EffectiveCsharp/Program.cs
+++ EffectiveCsharp/Program.cs
@@ -7,42 +7,8 @@ namespace EffectiveCsharp
     {
         static void Main(string[] args)
         {
-            Choujiang c = new Choujiang();
-            c.cj += c_choujiang;
-
-            Console.WriteLine("请填写你的名字");
-            string name = "";
-            while ((name=Console.ReadLine())!="")
-            {
-                c.CjFun(name);
-                 Console.WriteLine("请填写你的名字");
-            }
+            t14 t = new t14();
+            Console.WriteLine(t.copyName);
         }
-
-        static void c_choujiang(object sender, CjEventArg e)
-        {
-            Console.WriteLine($"{e.date}恭喜你中奖了");
-            Environment.Exit(0);
-        }
-    }
-
-    class Choujiang
-    {
-        public event EventHandler<CjEventArg> cj;
-        public void CjFun(string name)
-        {
-            if (name == "xx")
-            {
-                CjEventArg cc = new CjEventArg();
-                cc.date = DateTime.Now;
-                cj.Invoke(this,cc);
-               
-            }
-        }
-    }
-
-    class CjEventArg : EventArgs
-    {
-        public DateTime date;
     }
 }
